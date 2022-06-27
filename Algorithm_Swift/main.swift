53165b291e2f97c5d881c6bac830c462d21d8912
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-06-27 20:01:47 +0900

    m

--- Algorithm_Swift/main.swift
+++ Algorithm_Swift/main.swift
@@ -7,5 +7,40 @@
 
 import Foundation
 
-print("Hello, World!")
+let a = readLine()!
 
+var tag = ""
+var word = ""
+
+for char in a {
+    
+    if char == "<" {
+        
+        tag.append(char)
+        if !word.isEmpty {
+            print(String(word.reversed()),terminator: "")
+            word = ""
+        }
+        
+    } else if char == ">" {
+        
+        tag.append(char)
+        print(tag,terminator: "")
+        tag = ""
+        
+    } else {
+        
+        if tag.first == "<" {
+            tag.append(char)
+        } else if char == " " {
+            print(String(word.reversed()),terminator: " ")
+            word = ""
+        } else {
+            word.append(char)
+        }
+    }
+}
+
+if !word.isEmpty {
+    print(String(word.reversed()))
+}
